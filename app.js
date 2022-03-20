d731ef180bcdf8d02223bb6ac3dafb233ff3b506
EmilPaizano <pabloemilio.beta@gmail.com>
2022-03-19 22:35:30 -0600

    solution error CORS

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//CORSE para definir la whitelist
+var cors = require('cors')
+//extrae de las variables de entorno a CORS_ORIGIN que trae
+//consigo las URL que estaran en la whitelist
+var whiteList = (process.env.CORS_ORIGIN || '').split(',')
+
+//console.log(whiteList)
+var corsOptions = {
+    origin: (origin,callback)=>{
+        console.log("Origin value: ",origin)
+        if(whiteList.indexOf(origin) >= 0 ){
+            callback(null,true)
+        }else{
+            callback(new Error('CORS not Allowed'))
+        }
+    }
+}
 
 var indexRouter = require('./routes/index');
 var apiRouter = require("./routes/api/api");
@@ -10,6 +27,8 @@ var apiRouter = require("./routes/api/api");
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
